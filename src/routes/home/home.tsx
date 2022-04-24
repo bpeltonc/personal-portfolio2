d03eaacb9f3dcbb4795cd77bf9bbfa58b31d5936
Brandon Pelton-Cox <87504652+bpeltonc@users.noreply.github.com>
2022-04-24 16:26:36 -0500

    refactor: Adjust Home page containers
    This changes the containers around and in the home page
    to match the implementations of other pages

--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -3,18 +3,21 @@ import NavBar from "../../components/NavBar/NavBar";
 import Card from "../../components/Card/Card";
 
 import "./home.css"
+import React from "react";
 
 const Home: React.FC = () => {
     return (
-        <div className="main-container">
-            <Header title='Brandon Pelton-Cox' subtitle='Full-Stack Software Developer' color='white'/>
-            <NavBar dark={true} />
+        <React.Fragment>
+            <div className="header-container">
+                <Header title='Brandon Pelton-Cox' subtitle='Full-Stack Software Developer' color='white'/>
+                <NavBar dark={true} />
+            </div>
             <div className='carousel'>
                 <Card />
                 <Card />
                 <Card />
             </div>
-        </div>
+        </React.Fragment>
     );
 }
 
